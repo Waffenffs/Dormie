0aaebf6cfc36b073da32bb1ba98c1bef4d91ec9e
Waffen Sultan <waffen.sultan@gmail.com>
2024-10-18 16:30:13 +0800

    Fixes #5: fixed footer not being correctly placed

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -3,7 +3,7 @@ import Footer from "../components/footer";
 
 export default function HomePage() {
   return (
-    <main className="w-full h-full flex flex-col gap-5 flex-grow bg-background text-foreground">
+    <main className="w-full h-screen flex flex-col gap-5 flex-grow bg-background text-foreground">
       <section className="flex-grow my-12 md:my-24">
         <Hero />
       </section>
